4f76dd1d37240785ee81ea10b9cdbc9e14a9f682
William Liu <will123liu@gmail.com>
2024-04-16 10:09:52 -0700

    Create file

---
+++ fnirs-lab/trial.py
@@ -0,0 +1,14 @@
+"""
+This module defines the Trial object.
+
+The Trial object is the basis of fnirs-lab. It contains the data and metadata
+associated with a single recording.
+"""
+
+
+class Trial:
+    """Object to store fnirs data and metadata for a single recording.
+
+    """
+    def __init__(self) -> None:
+        pass
